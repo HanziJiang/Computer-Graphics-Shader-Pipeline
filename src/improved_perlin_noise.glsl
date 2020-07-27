2f8cd198049c6587a478807adef241bc3ebab367
Hanzi Jiang <hanzi.jiang@mail.utoronto.ca>
2020-07-26 22:06:13 -0400

    Rename variables

--- src/improved_perlin_noise.glsl
+++ src/improved_perlin_noise.glsl
@@ -16,33 +16,33 @@ float improved_perlin_noise( vec3 st)
   vec3 st_diff = st - st_lower;
   
   // Pseudorandom unit 3D vector starting from each grid point
-  vec3 c000 = random_direction(vec3(st_lower.x, st_lower.y, st_lower.z));
-  vec3 c100 = random_direction(vec3(st_upper.x, st_lower.y, st_lower.z));
-  vec3 c010 = random_direction(vec3(st_lower.x, st_upper.y, st_lower.z));
-  vec3 c110 = random_direction(vec3(st_upper.x, st_upper.y, st_lower.z));
-  vec3 c001 = random_direction(vec3(st_lower.x, st_lower.y, st_upper.z));
-  vec3 c101 = random_direction(vec3(st_upper.x, st_lower.y, st_upper.z));
-  vec3 c011 = random_direction(vec3(st_lower.x, st_upper.y, st_upper.z));
-  vec3 c111 = random_direction(vec3(st_upper.x, st_upper.y, st_upper.z));
+  vec3 gradient0 = random_direction(vec3(st_lower.x, st_lower.y, st_lower.z));
+  vec3 gradient1 = random_direction(vec3(st_upper.x, st_lower.y, st_lower.z));
+  vec3 gradient2 = random_direction(vec3(st_lower.x, st_upper.y, st_lower.z));
+  vec3 gradient3 = random_direction(vec3(st_upper.x, st_upper.y, st_lower.z));
+  vec3 gradient4 = random_direction(vec3(st_lower.x, st_lower.y, st_upper.z));
+  vec3 gradient5 = random_direction(vec3(st_upper.x, st_lower.y, st_upper.z));
+  vec3 gradient6 = random_direction(vec3(st_lower.x, st_upper.y, st_upper.z));
+  vec3 gradient7 = random_direction(vec3(st_upper.x, st_upper.y, st_upper.z));
   
   // Vectors from each grid point to st
-  vec3 p000 = vec3(st_diff.x, st_diff.y, st_diff.z);
-  vec3 p100 = vec3(st_diff.x - 1, st_diff.y, st_diff.z);
-  vec3 p010 = vec3(st_diff.x, st_diff.y - 1, st_diff.z);
-  vec3 p110 = vec3(st_diff.x - 1, st_diff.y - 1, st_diff.z);
+  vec3 distance0 = vec3(st_diff.x, st_diff.y, st_diff.z);
+  vec3 distance1 = vec3(st_diff.x - 1, st_diff.y, st_diff.z);
+  vec3 distance2 = vec3(st_diff.x, st_diff.y - 1, st_diff.z);
+  vec3 distance3 = vec3(st_diff.x - 1, st_diff.y - 1, st_diff.z);
   
-  vec3 p001 = vec3(st_diff.x, st_diff.y, st_diff.z - 1);
-  vec3 p101 = vec3(st_diff.x - 1, st_diff.y, st_diff.z - 1);
-  vec3 p011 = vec3(st_diff.x, st_diff.y - 1, st_diff.z - 1);
-  vec3 p111 = vec3(st_diff.x - 1, st_diff.y - 1, st_diff.z - 1);
+  vec3 distance4 = vec3(st_diff.x, st_diff.y, st_diff.z - 1);
+  vec3 distance5 = vec3(st_diff.x - 1, st_diff.y, st_diff.z - 1);
+  vec3 distance6 = vec3(st_diff.x, st_diff.y - 1, st_diff.z - 1);
+  vec3 distance7 = vec3(st_diff.x - 1, st_diff.y - 1, st_diff.z - 1);
   
   vec3 smooth_st_diff = vec3(improved_smooth_step(st_diff.x), improved_smooth_step(st_diff.y), improved_smooth_step(st_diff.z));
 
   // linear interpolation
-  float a = mix(dot(c000, p000), dot(c100, p100), smooth_st_diff.x);
-  float b = mix(dot(c010, p010), dot(c110, p110), smooth_st_diff.x);
-  float c = mix(dot(c001, p001), dot(c101, p101), smooth_st_diff.x);
-  float d = mix(dot(c011, p011), dot(c111, p111), smooth_st_diff.x);
+  float a = mix(dot(gradient0, distance0), dot(gradient1, distance1), smooth_st_diff.x);
+  float b = mix(dot(gradient2, distance2), dot(gradient3, distance3), smooth_st_diff.x);
+  float c = mix(dot(gradient4, distance4), dot(gradient5, distance5), smooth_st_diff.x);
+  float d = mix(dot(gradient6, distance6), dot(gradient7, distance7), smooth_st_diff.x);
   
   float e = mix(a, b, smooth_st_diff.y);
   float f = mix(c, d, smooth_st_diff.y);
